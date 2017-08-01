eeb2987c47f8bf79c1a64e778b593f75c935fd1d
Anthony Cabrera <arcabrera1@espe.edu.ec>
2017-08-01 12:00:45 -0500

    Commit Anthonyq
    Integrando las nuevas ventanas de Nico, falta usar el Scene Builder para los fxml

---
+++ ProjectMARTES  11h00/pass/src/pass/FXMain.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pass;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+/**
+ *
+ * @author nico
+ */
+public class FXMain extends Application {
+    
+    public static void main(String[] args) {
+        Application.launch(FXMain.class, args);
+    }
+   @Override
+   public void start(Stage primarystage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("login/Player.fxml"));        
+        Scene scene = new Scene(root);        
+        primarystage.setTitle("Login - [Grupo Delta]");
+        primarystage.setScene(scene);
+        primarystage.show();
+    }   
+}
