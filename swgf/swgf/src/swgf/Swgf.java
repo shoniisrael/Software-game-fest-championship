8d523d607ed95a497d314e6aeeb035397371f263
Shoni Israel <jivillacis4@espe.edu.ec>
2017-08-01 00:34:12 -0500

    Add files via upload

---
+++ swgf/swgf/src/swgf/Swgf.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swgf;
+
+/**
+ *
+ * @author qwerty
+ */
+public class Swgf {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
